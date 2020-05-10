118015fbf2a9e089f88fd48b03144e61c26361f9
Roger Generoso Masós <roger21gm@gmail.com>
2020-05-10 22:27:30 +0200

    Pre-created build

---
+++ build/precache-manifest.3486b0df55096838c2af7abcd557369d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219a16843c595b8d853bd14ec42a32cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d151dd4953b490c3d177",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8afcb3496b7f86cda4f1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d151dd4953b490c3d177",
+    "url": "/static/js/2.cd4219d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.cd4219d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8afcb3496b7f86cda4f1",
+    "url": "/static/js/main.c80c620f.chunk.js"
+  },
+  {
+    "revision": "e1772eb3ad915560e984",
+    "url": "/static/js/runtime-main.ffc8a2d8.js"
+  }
+]);
